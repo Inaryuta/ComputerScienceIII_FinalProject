4019305f4c52f9d4d3f685992b3287ce69e889ea
Slendeff <87665661+Slendeff@users.noreply.github.com>
2025-07-09 20:07:27 -0500

    si

--- code v1.0/gui_compiler.py
+++ code v1.0/gui_compiler.py
@@ -5,27 +5,27 @@ from compiler import Compiler
 class PianoCompilerGUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Compilador de Notas Musicales")
+        self.root.title("PianoDSL Compiler")
         self.compiler = Compiler()
 
         # Editor
-        self.editor = scrolledtext.ScrolledText(root, width=80, height=20, font=("Courier", 12))
+        self.editor = scrolledtext.ScrolledText(root, width=40, height=10, font=("Courier", 12))
         self.editor.pack(padx=10, pady=10)
 
         # Botón de Compilación
-        self.compile_button = tk.Button(root, text="Compilar y Visualizar", command=self.compile_code, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"))
+        self.compile_button = tk.Button(root, text="Compile and Visualize", command=self.compile_code, bg="#4CAF50", fg="white", font=("Arial", 12, "bold"))
         self.compile_button.pack(pady=(0,10))
 
     def compile_code(self):
         code = self.editor.get("1.0", tk.END).strip()
         if not code:
-            messagebox.showwarning("Advertencia", "Por favor escribe alguna instrucción.")
+            messagebox.showwarning("Warning", "Expecting instructions.")
             return
 
         try:
             self.compiler.compile_and_visualize(code)
         except SyntaxError as e:
-            messagebox.showerror("Error de Sintaxis", str(e))
+            messagebox.showerror("Sintax Error", str(e))
         except Exception as e:
             messagebox.showerror("Error", str(e))
 
